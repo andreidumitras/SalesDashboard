bb0d925da7064a5619a7ffd4434806373a8941ee
andreidumitras <andreei.dumitras@gmail.com>
2025-09-25 14:29:15 +0300

    feat(endpoints): add each page from figma

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-websiteBg">
+            <h1 className="text-4xl font-bold text-textLight mb-4">Landing page!!!</h1>
+            <p className="text-lg text-cardLight">Home sweet home</p>
+            <button className="mt-6 px-6 py-2 rounded bg-buttonLight text-white font-semibold shadow hover:bg-buttonDark transition">
+                Get Started
+            </button>
+        </div>
+    );
+}
